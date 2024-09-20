e15ba3b0c969808aad8e5d7ff7806e823b4ca1c5
Chesssfox <131539125+Chesssfox@users.noreply.github.com>
2024-09-20 15:48:25 +0800

    修复了摄像头边界的bug

--- camera/camera.gd
+++ camera/camera.gd
@@ -4,10 +4,12 @@ extends Camera2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var map_rect = tile_map.get_used_rect()
-	var tile_size = tile_map.cell_quadrant_size
-	var world_size_in_pixels = map_rect.size *tile_size
+	var tile_size = tile_map.rendering_quadrant_size
+	var world_size_in_pixels = map_rect.size * tile_size
 	limit_right = world_size_in_pixels.x
 	limit_bottom = world_size_in_pixels.y
+	limit_top = 0
+	limit_left = 0
 	
 
 
